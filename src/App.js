7b7de60a47a027647c403565c9718037aa630549
Mockelmann <ryguy.in.ne@gmail.com>
2023-09-21 03:53:47 -0500

    Beginning progress on header.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
+import Header2 from './components/Header2';
 
 function App() {
   return (
     <div className="App">
-      
+        <Header />
+        This is a test.<br/>
+        <Header2 />
+        another test.<br/>
+        <ul>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+            <li>aaa</li>
+        </ul>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import react from 'react';
+import ConvertDateToday from '../app/shared/FrenchTime';
+import { Col, Container, Row,  } from 'reactstrap';
+import '../app/assets/css/Home.css';
+import Logo from '../app/assets/img/m_fleur.png';
+
+const Header = () => {
+    return (
+        <header className='jumbotron jumbotron-fluid'>
+            <Container>
+                <Row className='text-right'>
+                    <Col className='text-warning mr-0 pr-1'>
+                        <ConvertDateToday />
+                    </Col>
+                </Row>
+                <Row>
+                <Col xs={3} lg={2}>
+                    <a href='/'><img src={Logo} fluid /></a>
+                </Col>
+                <Col xs className='align-self-center'>
+                    <h1 className='d-none d-md-block'>Le Saint du Mech</h1>
+                    <h2 className='d-md-none mt-3'>Le Saint du Mech</h2>
+                </Col>
+                </Row> 
+                xxx
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
+
+/*
+<header class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row text-right">
+                    <div class="col text-warning mr-0 pr-1 h6" id="dateFR"></div><img src="img/french.png" height="20px"/>
+                </div>
+                <div class="row">
+                    <div class="col-3 col-lg-2">
+                        <a href="index.html"><img src="img/m_fleur.png" class="img-fluid" /></a>
+                    </div>
+                    <div class="col align-self-center ">
+                        <h1 class="d-none d-md-block">Le Saint du Mech</h1>
+                        <h2 class="d-md-none mt-3">Le Saint du Mech</h2>
+                    </div>
+                </div> 
+            </div>
+        </header>
+*/
